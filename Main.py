6d72de121fcfae51ca19ff15729a67d21477d07e
Eros846 <karkisarik.2021@gmail.com>
2025-03-02 17:21:34 -0800

    Implemented skeleton code for the project

---
+++ Main.py
@@ -0,0 +1,131 @@
+import numpy as np
+
+# On the ith level of the search tree
+#--considering adding the -- feature
+#....
+#....
+
+#On the level i, I added feature 4 to current set ( the best feature)
+
+#On the i + 1 level of the search tree, with current set
+#--Considering adding the 1 feature
+#(Consider all other feature expect the one that's not on the current set)
+
+
+#On the level i+1, I added {best feature} to the current set ...
+
+#MATLAB CODE
+
+"""
+function accuracy = leave_one_out_cross_validation(data, current_set, feature_to_add)
+    accuracy = rand;
+end
+
+"""
+
+def leave_one_out_cross_validation(data, current_set, feature_to_add):
+    data = np.loadtxt(data)
+
+
+    number_correctly_classified = 0
+    # shape return row, colum
+    for i in range(1, data.shape[0]): #This loops over all the rows
+        object_to_classify = data[i, 1:]      #Current row and get all column except the 0th
+        label_object_to_classify = data[i][0] #First item to the row -- Given object
+
+
+        nearest_neighbor_distance = float('inf')
+        nearest_neighbor_location = float('inf')
+
+        for j  in range(1, data.shape[0]):
+            if j != i:
+                distance  = sqrt((sum((object_to_classify - data[k, 1:])))
+                if distance < nearest_neighbor_distance:
+                    nearest_neighbor_distance = distance
+                    nearest_neighbor_location = j
+                    nearest_neighbor_label = data[nearest_neighbor_location][0]
+
+
+
+        if label_object_to_classify == nearest_neightbor_label:
+            number_correctly_classified = number_correctly_classified + 1
+
+
+    accuracy = number_correctly_classfied = number_correctly_classified /size(data,1)
+
+
+
+
+
+
+#Search feature
+
+'''
+function feature_search_demo(data) 
+
+for i 1: size(data,2) -1 
+    disp(['On the ',num2str(i),'
+
+'''
+
+# def num2str(num):
+
+
+
+
+
+def feature_search(data):
+
+    current_set = set()
+
+    for i in range(1, data.shape[1]- 1):
+
+        print(f"On the {i}th level of the search tree")
+        feature_to_add_at_this_level = set()
+        best_accuracy = 0
+
+        for j in range(data.shape[1] -1):
+            if num2str(j) not in current_set:
+                print(f"--Considering adding the {num2str[j]} feature")
+                accuracy = leave_one_out_cross_validation(data, current_set, feature_to_add_at_this_level.pop(0))
+
+                if accuracy > best_accuracy:
+                    best_accuracy = accuracy
+                    feature_to_add_at_this_level = k
+
+        current_set_of_features[i] = feature_to_add_at_this_level
+        print(f"On this level {num2str[i]} i added feature, {num2str(feature_to_add_at_this_level)}")
+
+
+
+def main():
+    print("Welcome to Feature Search Algorithm.")
+    inputFile = input(print("Type in the name of the file to test: "))
+
+    print()
+    print("Type the number of the algorithm you want to run.")
+    print()
+    print("     1)Forward Selection")
+    print("     2)Backward Selection")
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+            
+
+
+
+
+
+
+
